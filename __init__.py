89ab342e4b18863f3220804c83b29bcad3653cf4
fsdymy1024 <471865477@qq.com>
2024-05-10 14:41:26 +0800

    x

---
+++ __init__.py
@@ -0,0 +1,6 @@
+from .nodes import SaveImageWithoutMetadata
+
+
+NODE_CLASS_MAPPINGS = {"Save Image Without Metadata": SaveImageWithoutMetadata,}
+
+NODE_DISPLAY_NAME_MAPPINGS = {"Save Image Without Metadata": "Save Image Without Metadata❌"}
---
+++ nodes.py
@@ -0,0 +1,53 @@
+import os
+import folder_paths
+from PIL import Image
+import numpy as np
+
+
+class SaveImageWithoutMetadata:
+    def __init__(self):
+        self.output_dir = folder_paths.get_output_directory()
+        self.type = "output"
+        self.prefix_append = ""
+        self.compress_level = 4
+
+    @classmethod
+    def INPUT_TYPES(s):
+        return {"required": 
+                    {"images": ("IMAGE", ),
+                     "filename_prefix": ("STRING", {"default": "ComfyUInoMetadata"})},
+                "hidden": {"prompt": "PROMPT", "extra_pnginfo": "EXTRA_PNGINFO"},
+                }
+
+    RETURN_TYPES = ()
+    FUNCTION = "save_images"
+
+    OUTPUT_NODE = True
+
+    CATEGORY = "Fsdymy"
+
+    def save_images(self, images, filename_prefix="ComfyUInoMetadata", prompt=None, extra_pnginfo=None):
+        filename_prefix += self.prefix_append
+        full_output_folder, filename, counter, subfolder, filename_prefix = folder_paths.get_save_image_path(filename_prefix, self.output_dir, images[0].shape[1], images[0].shape[0])
+        results = list()
+        for (batch_number, image) in enumerate(images):
+            i = 255. * image.cpu().numpy()
+            img = Image.fromarray(np.clip(i, 0, 255).astype(np.uint8))
+            metadata = None
+            filename_with_batch_num = filename.replace("%batch_num%", str(batch_number))
+            file = f"{filename_with_batch_num}_{counter:05}_.png"
+            img.save(os.path.join(full_output_folder, file), pnginfo=metadata, compress_level=self.compress_level)
+            results.append({
+                "filename": file,
+                "subfolder": subfolder,
+                "type": self.type
+            })
+            counter += 1
+
+        return { "ui": { "images": results } }
+
+
+
+NODE_CLASS_MAPPINGS = {
+    "Save Image Without Metadata": SaveImageWithoutMetadata,
+}
